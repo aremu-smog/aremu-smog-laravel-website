b31d60b309bb9bb49ce0383fc2e3bbef41afab68
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2020-01-15 21:41:04 +0100

    My Mind setup

--- resources/views/my-mind.blade.php
+++ resources/views/my-mind.blade.php
@@ -6,8 +6,45 @@
 
 @section('page-content')
 
-<div class="about-me col-8">
-       
+<div class="my-mind col-10">
+       <div class="row recent-articles">
+          
+          <div class="col-4">
+               <article class="article">
+                  <header>
+                     <img src="/img/my-mind/DevFest Aba - Aremu Smog.png" alt="">
+                  </header>
+                  <section>Giving the first speech at Devfest Aba, 2019</section>
+                  <footer>
+                     Published on Dec 13, 2019 - 5min read
+                  </footer>
+               </article>
+            </div>
+
+            <div class="col-4">
+                  <article class="article">
+                     <header>
+                        <img src="/img/my-mind/iya-alata.png" alt="">
+                     </header>
+                     <section>What I do instead of free jobs(websites, graphic design and...)</section>
+                     <footer>
+                        Published on Dec 13, 2019 - 5min read
+                     </footer>
+                  </article>
+            </div>
+
+            <div class="col-4">
+                  <article class="article">
+                     <header>
+                        <img src="/img/my-mind/Reactjs.png" alt="">
+                     </header>
+                     <section>Working with local images in ReactJS</section>
+                     <footer>
+                        Published on Dec 13, 2019 - 5min read
+                     </footer>
+                  </article>
+            </div>
+       </div>
 </div>
     
 @endsection
