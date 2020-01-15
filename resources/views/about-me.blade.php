625ffac5ca59f8919e670a666bd8b3c14e5060a0
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2020-01-15 23:03:37 +0100

    About me updated

--- resources/views/about-me.blade.php
+++ resources/views/about-me.blade.php
@@ -7,8 +7,15 @@
 @section('page-content')
 
 <div class="about-me col-8">
-        I have over the past five(5) years helped built identity and websites for small and medium sized enterprises. Over the same year, I have flexed my muscle in the areas of entrepreneurship and starting a business but it has always ended in tears: meaning there is a lot I still need to learn in this domain. Presently, 
-        I spend on my time and energy horning my skills as a web developer (Frontend and Backend). Languages and Frameworks I am familiar with are
+        I have over the past five(5) years  built identity and websites for small and medium sized enterprises. Over the same period, I have flexed my muscle in the areas of entrepreneurship and starting a business but it has always ended in tears{{'😭'}}: meaning there is a lot I still need to learn in this domain. Presently, 
+        I spend all my time and energy in horning my skills as a web developer (Frontend and Backend). Languages and Frameworks I am familiar with include
+        <div class="frameworks">
+            <img src="img/main/html5.svg" alt="HTML5">
+            <img src="img/main/JQuery_logo.png" alt="jQuery">
+            <img src="img/main/css3.svg" alt="CSS3">
+            <img src="img/main/ReactJS.png" alt="ReactJS">
+            <img src="img/main/Laravel.png" alt="Laravel">
+        </div>
 </div>
     
 @endsection
