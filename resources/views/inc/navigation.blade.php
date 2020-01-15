ac040016e2cb0afc0d9a5bb670ca00ebd600e1e4
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2020-01-15 16:37:01 +0100

    Fix navigation style

--- resources/views/inc/navigation.blade.php
+++ resources/views/inc/navigation.blade.php
@@ -1,13 +1,14 @@
 <div class="navigation">
-<span class="{{Request::is('about-me') ? "hide-nav":""}}"><a href="/about-me">About me</a> </span>
-    <span class="{{Request::is('portfolio') ? "hide-nav":""}}"><a href="/portfolio">Portfolio</a></span>
-    <br/>
-    <span class="{{Request::is('hire-me') ? "hide-nav":""}}"><a href="/hire-me">Hire me</a> </span>
-    <span class="{{Request::is('reading-list') ? "hide-nav":""}}"><a href="/reading-list">Reading List</a></span>
-    <span class="{{Request::is('my-mind') ? "hide-nav":""}}"><a href="/my-mind">My Mind</a></span>
+    <nav> 
+        <span class="{{Request::is('about-me') ? "hide-nav":""}}"><a href="/about-me">About me</a> </span>
+        <span class="{{Request::is('portfolio') ? "hide-nav":""}}"><a href="/portfolio">Portfolio</a></span>
+        <span class="{{Request::is('hire-me') ? "hide-nav":""}}"><a href="/hire-me">Hire me</a> </span>
+        <span class="{{Request::is('reading-list') ? "hide-nav":""}}"><a href="/reading-list">Reading List</a></span>
+        <span class="{{Request::is('my-mind') ? "hide-nav":""}}"><a href="/my-mind">My Mind</a></span>
+    </nav>
     <div class="social">
         <a href="http://instagram.com/aremu_smog" target="_blank"><img src="/img/social/instagram.png" alt="Instagram logo"></a>
         <a href="http://twiter.com/aremu_smog" target="_blank"><img src="/img/social/twitter.png" alt="Twitter logo"></a>
-        <a href="" target="_blank"><img src="/img/social/facebook.png" alt="Facebook logo"></a>
+        <a href="https://web.facebook.com/aremu.gbamila" target="_blank"><img src="/img/social/facebook.png" alt="Facebook logo"></a>
     </div>
 </div>
